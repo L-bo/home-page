1dfa4c7beef137136b1246003643d59fdd7ede4b
yourname <1300547457@qq.com>
2020-01-10 22:46:46 +0800

    ../

---
+++ src/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main-div">
+    <img src="../assets/background_vue.png" style="display: block" width="100%" height="100%"/>
+  </div>
+  <!--<div>-->
+    <!--<img src="../assets/background_vue.png" width="100%" height="100%"/>-->
+    <!--<router-view/>-->
+  <!--</div>-->
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+<style scoped>
+ main-div{
+   width: 100%;
+   height: 100%;
+   background: image(royalblue);
+ }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
+/* eslint-disable no-undef */
 import Vue from 'vue'
 import Router from 'vue-router'
+import Main from '@/components/Main'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'Main',
+      component: Main
+    },
     {
       path: '/',
       name: 'HelloWorld',
